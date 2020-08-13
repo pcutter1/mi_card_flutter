524995d968729977a9154d6ef8484d9aeae21f94
Paul C <pcutter@cnm.edu>
2020-08-13 17:35:11 -0600

    Testing spacing formatting between children

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,36 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 200.0,
-            margin: EdgeInsets.all(20.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.orangeAccent,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.lightBlueAccent,
+                child: Text('Container 2'),
+              ),
+              SizedBox(height: 20,
+              ),
+              Container(
+                height: 100.0,
+                width: 200.0,
+                color: Colors.deepPurpleAccent,
+                child: Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+              )
+            ],
           ),
         ),
       ),
