b44aec7b97021240f6f69f6137727eed8fddcc11
Paul C <pcutter@cnm.edu>
2020-08-13 17:09:13 -0600

    Toying around with formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 200.0,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
