bbd738cb9c5537af3b634365e09925e83289adab
Paul C <pcutter@cnm.edu>
2020-08-14 16:49:32 -0600

    Completed business card app! (color tweak)

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey[200],
+        backgroundColor: Colors.grey[100],
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
