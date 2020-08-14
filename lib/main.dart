3d10bc4cf41fdbc383b1aeb438c4bada5acc7fce
Paul C <pcutter@cnm.edu>
2020-08-14 15:35:59 -0600

    Added custom font and icons

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,64 @@ class MyApp extends StatelessWidget {
                 'Paul Quitzau Cutter',
                 style: TextStyle(
                   fontFamily: 'Barcode',
-                  fontSize: 30.0,
+                  fontSize: 35.0,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Roboto Mono',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.0,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '505.401.8656',
+                      style: TextStyle(
+                        fontFamily: 'Roboto Mono',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'pcutter@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Roboto Mono',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
